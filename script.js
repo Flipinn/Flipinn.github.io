9ef9999870f2e0dde52b8b8ec3682e2e7864ae32
Flipinn <lucascampodonico2@gmail.com>
2024-09-13 03:12:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const btnNo = document.querySelector("#btn-random")
+
+function moverAleatoriamente(btn) {
+    btn.style.position = "absolute";
+    btn.style.fontWeight = "bolder";
+    btn.style.top = Math.floor(Math.random() * 90 + 5) + "%"
+    btn.style.left = Math.floor(Math.random() * 90 + 5) + "%"
+}
+
+btnNo.addEventListener("mouseenter", function (e) {
+    moverAleatoriamente(e.target)
+})
+function destino(tu, yo) {
+    return `Estamos destinados a estar juntos como ${tu} y ${yo}.`;
+}
+
+console.log(destino(`HTML`, `CSS`));
